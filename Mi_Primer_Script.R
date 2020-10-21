0b149117e67329519ef8f444ecf3d9c34b1317ce
JeshuaVC <jeshua.9451@gmail.com>
2020-10-21 12:03:44 -0500

    Este es mi primer script. Me siento bien, ya que despues de estar estresado por dias pude realizar y controlar un poco esto.

---
+++ Mi_Primer_Script.R
@@ -0,0 +1,7 @@
+#1.- Vamos a crear un vector que contenfa 5 variables cuantitativas discretas,
+#contenidas en un variable llamada muestra 
+
+muestra <-c (10,35,20,18,15)
+
+#2.- Vamos a calcular la media 
+mean(muestra)
